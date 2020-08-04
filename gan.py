f03189a2b180ed60d4422e53931395067d19438b
Tao <huangtao@videopls.com>
2020-08-04 16:11:59 +0800

    baseline.

--- gan.py
+++ gan.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-
+import numpy as np
 import argparse
 import time
 from os.path import join as ops
@@ -15,21 +15,21 @@ parser = argparse.ArgumentParser(
 train_set = parser.add_mutually_exclusive_group()
 parser.add_argument('--batch_size', default=64, type=int,
                     help='Batch size for training')
-parser.add_argument('--gnet', type=str, default='checkpoints/g/Pokemonet_sg.pthone',
+parser.add_argument('--gnet', type=str, default='checkpoints/Pokemonet_sg.pth010',
                     help='Checkpoint state_dict file to resume training from')
-parser.add_argument('--dnet', type=str, default='checkpoints/d/Pokemonet_sd.pth400',
+parser.add_argument('--dnet', type=str, default='checkpoints/Pokemonet_sd.pth010',
                     help='Checkpoint state_dict file to resume training from')
 parser.add_argument('--epochs', '-e', default=800, type=int,
                     help='the number of training epochs')
 parser.add_argument('--start_iter', default=0, type=int,
                     help='Resume training at this iter')
-parser.add_argument('--lr', '--learning-rate', default=1e-5, type=float,
+parser.add_argument('--lr', '--learning-rate', default=1e-4, type=float,
                     help='initial learning rate')
 args = parser.parse_args()
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
 
-def train_dnet(loader, sgnet, sdnet, optimizer):
+def train_dnet(loader, sgnet, sdnet, optimizer, N):
     loss_amount = 0
     # load train data
     for iteration, batch in enumerate(loader):
@@ -45,7 +45,7 @@ def train_dnet(loader, sgnet, sdnet, optimizer):
         preds_r = sdnet(Pokémon).mean()
         preds_f = sdnet(fakePoké).mean()
 
-        loss = - preds_r * 0.6 + preds_f
+        loss = - preds_r * N + preds_f
         loss.backward()
         optimizer.step()
         loss_amount += loss.item()
@@ -112,12 +112,12 @@ def train():
     # optimizerG = optim.SGD(Gnet.parameters(), lr=args.lr, momentum=0.9,
     #                       weight_decay=5e-4)
 
-    optimizerD = optim.SGD(filter(lambda p: p.requires_grad, sdnet.parameters()), lr=args.lr*4, momentum=0.9,
-                          weight_decay=5e-4)
-    optimizerG = optim.SGD(sgnet.parameters(), lr=args.lr, momentum=0.9,
-                          weight_decay=5e-4)
-    # optimizerD = optim.Adam(filter(lambda p: p.requires_grad, sdnet.parameters()), lr=args.lr*4,betas=(0.5,0.999))
-    # optimizerG = optim.Adam(sgnet.parameters(),lr=args.lr,betas=(0.5,0.999))
+    # optimizerD = optim.SGD(filter(lambda p: p.requires_grad, sdnet.parameters()), lr=args.lr*4, momentum=0.9,
+    #                       weight_decay=5e-4)
+    # optimizerG = optim.SGD(sgnet.parameters(), lr=args.lr, momentum=0.9,
+    #                       weight_decay=5e-4)
+    optimizerD = optim.Adam(filter(lambda p: p.requires_grad, sdnet.parameters()), lr=args.lr*4,betas=(0.5,0.999))
+    optimizerG = optim.Adam(sgnet.parameters(),lr=args.lr,betas=(0.5,0.999))
     # criterion = nn.SmoothL1Loss().cuda()
 
     print('Loading the dataset...')
@@ -131,12 +131,13 @@ def train():
     # Dnet.train()
     # Gnet.train()
     # create batch iterator
+    for _ in range(5):
+        loss_d = train_dnet(PokéBall, sgnet, sdnet, optimizerD, 1.)
     for iteration in range(args.start_iter + 1, args.epochs):
         print('Epoch %d' % iteration, end=' ')
         # train_encoder_decoder(PokéBall, Dnet, Gnet, criterion, optimizerD, optimizerG)
-        
-        loss_d = train_dnet(PokéBall, sgnet, sdnet, optimizerD)
-        for i in range(5):
+        loss_d = train_dnet(PokéBall, sgnet, sdnet, optimizerD, 0.7)
+        for i in range(2):
             loss_g = train_gnet(PokéBall, sgnet, sdnet, optimizerG)
         data.shuffle()
         if not (iteration-args.start_iter) == 0 and iteration % 10 == 0:
